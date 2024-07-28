e655b5194d7374ec2e7b570a395d47ae7a2693d1
Lavanya989 <lavanya.marthanapalle@ibm.com>
2024-07-28 13:31:51 +0530

    Add files via upload
    Activities from 1 to 8

---
+++ SQL/Activities/sqlscript_1_2_3_4_5.sql
@@ -0,0 +1,347 @@
+REM   Script: Activity 1, 2, 3, 4, 5
+REM   SQL Activity from 1 to 5
+
+DROP table salesmanLav;
+
+Create table salesmanLav(salesman_id int PRIMARY KEY, salesman_name varchar2(25) NOT NULL, salesman_city varchar(20), comission int);
+
+DESCRIBE salesmanLav
+
+
+DROP table salesmanLav;
+
+Create table salesmanLav(salesman_id int PRIMARY KEY, salesman_name varchar2(25) NOT NULL, salesman_city varchar(20), comission int);
+
+DESCRIBE salesmanLav
+
+
+Insert into salesmanLav("salesman_id", "slaesman_name", "salesman_city", "comission") values(1001, 'Lavanya', 'Hyderabad', 20) 
+--Insert into salesmanLav(salesman_id, slaesman_name, salesman_city, comission) values(1002, 'Kanth', 'Bengaluru', 40) 
+--Insert into salesmanLav(salesman_id, slaesman_name, salesman_city, comission) values(1003, 'Kiaan', 'Chennai', 35) 
+--Insert into salesmanLav(salesman_id, slaesman_name, salesman_city, comission) values(1004, 'Krish', 'Hyderabad',10) 
+ 
+Select * from salesmanLav;
+
+DROP table salesmanLav;
+
+Create table salesmanLav(salesman_id int PRIMARY KEY, salesman_name varchar2(25) NOT NULL, salesman_city varchar(20), comission int);
+
+DESCRIBE salesmanLav
+
+
+Insert into salesmanLav("salesman_id", "slaesman_name", "salesman_city", "comission") values(1001, 'Lavanya', 'Hyderabad', 20);
+
+Select * from salesmanLav;
+
+DROP table salesmanLav;
+
+Create table salesmanLav(salesman_id int PRIMARY KEY, salesman_name varchar2(25) NOT NULL, salesman_city varchar(20), comission int);
+
+DESCRIBE salesmanLav
+
+
+Insert into salesmanLav("salesman_id", "slaesman_name", "salesman_city", "comission") values(1001, 'Lavanya', 'Hyderabad', '20');
+
+Select * from salesmanLav;
+
+DROP table salesmanLav;
+
+Create table salesmanLav(salesman_id int PRIMARY KEY, salesman_name varchar2(25) NOT NULL, salesman_city varchar(20), comission int);
+
+DESCRIBE salesmanLav
+
+
+Insert into salesmanLav("salesman_id", "slaesman_name", "salesman_city", "comission") values(1001, 'Lavanya', 'Hyderabad', 20);
+
+Select * from salesmanLav;
+
+DROP table salesmanLav;
+
+Create table salesmanLav(salesman_id int PRIMARY KEY, salesman_name varchar2(25) NOT NULL, salesman_city varchar(20), comission int);
+
+DESCRIBE salesmanLav
+
+
+Insert into salesmanLav("salesman_id", "slaesman_name", "salesman_city", "comission") values(1001, 'Lavanya', 'Hyderabad', 20);
+
+Select * from salesmanLav;
+
+INSERT ALL 
+    INTO salesmanLav VALUES(1002, 'Kanth', 'Hyd',60) 
+ INTO salesmanLav VALUES(1003, 'Kiaan', 'Chennai',80) 
+SELECT 1 FROM DUAL;
+
+SELECT * FROM salesmanLav;
+
+DROP table salesmanLav;
+
+Create table salesmanLav(salesman_id int PRIMARY KEY, salesman_name varchar2(25) NOT NULL, salesman_city varchar(20), comission int);
+
+DESCRIBE salesmanLav
+
+
+Insert into salesmanLav("salesman_id", "salesman_name", "salesman_city", "comission") values(1001, 'Lavanya', 'Hyderabad', 20);
+
+Select * from salesmanLav;
+
+INSERT ALL 
+    INTO salesmanLav VALUES(1002, 'Kanth', 'Hyd',60) 
+ INTO salesmanLav VALUES(1003, 'Kiaan', 'Chennai',80) 
+SELECT 1 FROM DUAL;
+
+SELECT * FROM salesmanLav;
+
+DROP table salesmanLav;
+
+Create table salesmanLav(salesman_id int PRIMARY KEY, salesman_name varchar2(25) NOT NULL, salesman_city varchar(20), comission int);
+
+DESCRIBE salesmanLav
+
+
+Insert into salesmanLav("salesman_id", "salesman_name", "salesman_city", "comission") values(1001, 'Lavanya', 'Hyderabad', 20);
+
+Select * from salesmanLav;
+
+INSERT ALL 
+    INTO salesmanLav VALUES(1002, 'Kanth', 'Hyd',60) 
+ INTO salesmanLav VALUES(1003, 'Kiaan', 'Chennai',80) 
+SELECT 1 FROM DUAL;
+
+SELECT * FROM salesmanLav;
+
+DROP table salesmanLav;
+
+Create table salesmanLav(salesman_id int PRIMARY KEY, salesman_name varchar2(25) NOT NULL, salesman_city varchar(20), comission int);
+
+DESCRIBE salesmanLav
+
+
+Insert into salesmanLav("salesman_id", "salesman_name", "salesman_city", comission) values(1001, 'Lavanya', 'Hyderabad', 20);
+
+Select * from salesmanLav;
+
+INSERT ALL 
+    INTO salesmanLav VALUES(1002, 'Kanth', 'Hyd',60) 
+ INTO salesmanLav VALUES(1003, 'Kiaan', 'Chennai',80) 
+SELECT 1 FROM DUAL;
+
+SELECT * FROM salesmanLav;
+
+DROP table salesmanLav;
+
+Create table salesmanLav(salesman_id int PRIMARY KEY, salesman_name varchar2(25) NOT NULL, salesman_city varchar(20), comission int);
+
+DESCRIBE salesmanLav
+
+
+Insert into salesmanLav(salesman_id, salesman_name, salesman_city, comission) values(1001, 'Lavanya', 'Hyderabad', 20);
+
+Select * from salesmanLav;
+
+INSERT ALL 
+    INTO salesmanLav VALUES(1002, 'Kanth', 'Hyd',60) 
+ INTO salesmanLav VALUES(1003, 'Kiaan', 'Chennai',80) 
+SELECT 1 FROM DUAL;
+
+SELECT * FROM salesmanLav;
+
+DROP table salesmanLav;
+
+Create table salesmanLav(salesman_id int PRIMARY KEY, salesman_name varchar2(25) NOT NULL, salesman_city varchar(20), comission int);
+
+DESCRIBE salesmanLav
+
+
+Insert into salesmanLav(salesman_id, salesman_name, salesman_city, comission) values(5001, 'James Hoog', 'New York', 15);
+
+Select * from salesmanLav;
+
+INSERT ALL 
+    INTO salesmanLav VALUES(5002, 'Nail Knite', 'Paris',13) 
+    INTO salesmanLav VALUES(5005, 'Pit Alex', 'London',11) 
+     INTO salesmanLav VALUES(5006, 'McLyon', 'Paris',14) 
+     INTO salesmanLav VALUES(5007, 'Paul Adam', 'Rome',13) 
+     INTO salesmanLav VALUES(5003, 'Lauson Hen', 'San Jose',12) 
+     
+SELECT 1 FROM DUAL;
+
+SELECT * FROM salesmanLav;
+
+DROP table salesmanLav;
+
+Create table salesmanLav(salesman_id int PRIMARY KEY, salesman_name varchar2(25) NOT NULL, salesman_city varchar(20), comission int);
+
+DESCRIBE salesmanLav
+
+
+Insert into salesmanLav(salesman_id, salesman_name, salesman_city, comission) values(5001, 'James Hoog', 'New York', 15);
+
+Select * from salesmanLav;
+
+INSERT ALL 
+    INTO salesmanLav VALUES(5002, 'Nail Knite', 'Paris',13) 
+    INTO salesmanLav VALUES(5005, 'Pit Alex', 'London',11) 
+     INTO salesmanLav VALUES(5006, 'McLyon', 'Paris',14) 
+     INTO salesmanLav VALUES(5007, 'Paul Adam', 'Rome',13) 
+     INTO salesmanLav VALUES(5003, 'Lauson Hen', 'San Jose',12) 
+     
+SELECT 1 FROM DUAL;
+
+SELECT * FROM salesmanLav;
+
+SELECT salesman_id , salesman_name from salesmanLav ;
+
+SELECT * from salesmanLav where salesman_city = 'Paris';
+
+DROP table salesmanLav;
+
+Create table salesmanLav(salesman_id int PRIMARY KEY, salesman_name varchar2(25) NOT NULL, salesman_city varchar(20), comission int);
+
+DESCRIBE salesmanLav
+
+
+Insert into salesmanLav(salesman_id, salesman_name, salesman_city, comission) values(5001, 'James Hoog', 'New York', 15);
+
+Select * from salesmanLav;
+
+INSERT ALL 
+    INTO salesmanLav VALUES(5002, 'Nail Knite', 'Paris',13) 
+    INTO salesmanLav VALUES(5005, 'Pit Alex', 'London',11) 
+     INTO salesmanLav VALUES(5006, 'McLyon', 'Paris',14) 
+     INTO salesmanLav VALUES(5007, 'Paul Adam', 'Rome',13) 
+     INTO salesmanLav VALUES(5003, 'Lauson Hen', 'San Jose',12) 
+     
+SELECT 1 FROM DUAL;
+
+SELECT * FROM salesmanLav;
+
+SELECT salesman_id , salesman_name from salesmanLav ;
+
+SELECT * from salesmanLav where salesman_city = 'Paris';
+
+SELECT salesman_id , comission from salesmanLav where salesman_name = 'Paul Adam';
+
+SELECT salesman_id , salesman_name from salesmanLav ;
+
+SELECT * from salesmanLav where salesman_city = 'Paris';
+
+SELECT salesman_id , comission from salesmanLav where salesman_name = 'Paul Adam';
+
+ALTER table salesmanLav ADD (grade int);
+
+UPDATE table salesmanLav SET grade=100;
+
+SELECT * from salesmanLav;
+
+ALTER table salesmanLav ADD (grade int);
+
+UPDATE salesmanLav SET grade=100;
+
+SELECT * from salesmanLav;
+
+DROP table salesmanLav;
+
+Create table salesmanLav(salesman_id int PRIMARY KEY, salesman_name varchar2(25) NOT NULL, salesman_city varchar(20), comission int);
+
+DESCRIBE salesmanLav
+
+
+Insert into salesmanLav(salesman_id, salesman_name, salesman_city, comission) values(5001, 'James Hoog', 'New York', 15);
+
+Select * from salesmanLav;
+
+INSERT ALL 
+    INTO salesmanLav VALUES(5002, 'Nail Knite', 'Paris',13) 
+    INTO salesmanLav VALUES(5005, 'Pit Alex', 'London',11) 
+     INTO salesmanLav VALUES(5006, 'McLyon', 'Paris',14) 
+     INTO salesmanLav VALUES(5007, 'Paul Adam', 'Rome',13) 
+     INTO salesmanLav VALUES(5003, 'Lauson Hen', 'San Jose',12) 
+     
+SELECT 1 FROM DUAL;
+
+SELECT * FROM salesmanLav;
+
+SELECT salesman_id , salesman_name from salesmanLav ;
+
+SELECT * from salesmanLav where salesman_city = 'Paris';
+
+SELECT salesman_id , comission from salesmanLav where salesman_name = 'Paul Adam';
+
+ALTER table salesmanLav ADD (grade int);
+
+UPDATE salesmanLav SET grade=100;
+
+SELECT * from salesmanLav;
+
+UPDATE salesmanLav SET grade=200 where salesman_city = Rome;
+
+UPDATE salesmanLav SET grade=300 where salesman_name = 'James Hoog' 
+--Update the name McLyon to Pierre. 
+UPDATE salesmanLav SET salesman_name = pierre where salesman_name = McLyon;
+
+Select * from salesmanLav;
+
+DROP table salesmanLav;
+
+Create table salesmanLav(salesman_id int PRIMARY KEY, salesman_name varchar2(25) NOT NULL, salesman_city varchar(20), comission int);
+
+DESCRIBE salesmanLav
+
+
+Insert into salesmanLav(salesman_id, salesman_name, salesman_city, comission) values(5001, 'James Hoog', 'New York', 15);
+
+Select * from salesmanLav;
+
+INSERT ALL 
+    INTO salesmanLav VALUES(5002, 'Nail Knite', 'Paris',13) 
+    INTO salesmanLav VALUES(5005, 'Pit Alex', 'London',11) 
+     INTO salesmanLav VALUES(5006, 'McLyon', 'Paris',14) 
+     INTO salesmanLav VALUES(5007, 'Paul Adam', 'Rome',13) 
+     INTO salesmanLav VALUES(5003, 'Lauson Hen', 'San Jose',12) 
+     
+SELECT 1 FROM DUAL;
+
+SELECT * FROM salesmanLav;
+
+SELECT salesman_id , salesman_name from salesmanLav ;
+
+SELECT * from salesmanLav where salesman_city = 'Paris';
+
+SELECT salesman_id , comission from salesmanLav where salesman_name = 'Paul Adam';
+
+ALTER table salesmanLav ADD (grade int);
+
+UPDATE salesmanLav SET grade=100;
+
+SELECT * from salesmanLav;
+
+UPDATE salesmanLav SET grade=200 where salesman_city = 'Rome';
+
+UPDATE salesmanLav SET grade=300 where salesman_name = 'James Hoog' 
+--Update the name McLyon to Pierre. 
+UPDATE salesmanLav SET salesman_name = pierre where salesman_name = 'McLyon';
+
+Select * from salesmanLav;
+
+UPDATE salesmanLav SET grade=200 where salesman_city = 'Rome';
+
+UPDATE salesmanLav SET grade=300 where salesman_name = 'James Hoog' 
+--Update the name McLyon to Pierre. 
+UPDATE salesmanLav SET salesman_name = pierre where salesman_name = 'McLyon';
+
+UPDATE salesmanLav SET grade=200 where salesman_city = 'Rome';
+
+UPDATE salesmanLav SET grade=300 where salesman_name = 'James Hoog';
+
+UPDATE salesmanLav SET salesman_name = pierre where salesman_name = 'McLyon';
+
+Select * from salesmanLav;
+
+UPDATE salesmanLav SET grade=200 where salesman_city = 'Rome';
+
+UPDATE salesmanLav SET grade=300 where salesman_name = 'James Hoog';
+
+UPDATE salesmanLav SET salesman_name = 'pierre' where salesman_name = 'McLyon';
+
+Select * from salesmanLav;
+
